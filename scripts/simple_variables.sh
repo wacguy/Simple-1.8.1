3a46dcb92e2c29ee7fa4cb6e200c99d18d878a00
Guy Wachsman <guywachsman@mbp.local>
2018-08-31 15:33:41 -0400

    .
    add * b4 mut & wt to include name of files

--- scripts/simple_variables.sh
+++ scripts/simple_variables.sh
@@ -6,12 +6,12 @@
 java='/usr/lib/jvm/java-1.8.0/bin/java'
 
 #input files
-mut_files=fastq/mut*
-wt_files=fastq/wt*
+mut_files=fastq/*mut*
+wt_files=fastq/*wt*
 
 #names
 mutation=recessive #change to dominant if the mutation is dominant
-line=EMS  ##if you prefer, change EMS to the name of your line.  Letters and underscores only.
+line=EMS_995  ##if you prefer, change EMS to the name of your line.  Letters and underscores only.
 mut=EMS_mut
 wt=EMS_wt
 my_species=Arabidopsis_thaliana ####################paste your species name here to replace Arabidopsis_thaliana
